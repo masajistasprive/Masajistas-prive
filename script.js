64d1d2e4fd1215ea2b16f8bf7c2e98a5c579ebb8
masajistasprive <masajistasprive@gmail.com>
2026-09-09 10:14:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -574,7 +574,7 @@ function openProfileById(id) {
 
   const container = document.getElementById('modal-gallery-container');
   container.innerHTML = '';
-  
+  container.scrollLeft = 0;
   p.images.forEach((imgSrc, index) => {
     const wrapper = document.createElement('div');
     wrapper.className = 'img-wrapper';
